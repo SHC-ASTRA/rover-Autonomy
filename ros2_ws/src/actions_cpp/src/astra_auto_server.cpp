121adf07b4d09dc78486b0cc9a7860f9b99170a1
Daegan Brown <daeganbrown03@gmai.com>
2024-03-01 17:12:20 -0600

    removed opencv2/highgui.hpp for potential errors

--- ros2_ws/src/actions_cpp/src/astra_auto_server.cpp
+++ ros2_ws/src/actions_cpp/src/astra_auto_server.cpp
@@ -32,7 +32,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/aruco.hpp>
 #include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
+//#include <opencv2/highgui.hpp>
 #include <opencv2/objdetect/aruco_detector.hpp>
 #include <opencv2/calib3d.hpp>
 //#include <opencv2/aruco_samples_utlity.hpp>
