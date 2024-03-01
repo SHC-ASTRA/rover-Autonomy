b24f19468e6152bb8dc058421547d9ede6701620
Daegan Brown <daeganbrown03@gmai.com>
2024-03-01 15:19:56 -0600

    LETS FUCKING GO

--- ros2_ws/src/actions_cpp/src/astra_auto_server.cpp
+++ ros2_ws/src/actions_cpp/src/astra_auto_server.cpp
@@ -29,9 +29,13 @@
 
 //openCV shenanigans
 #include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
 #include <opencv2/aruco.hpp>
 #include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 #include <opencv2/objdetect/aruco_detector.hpp>
+#include <opencv2/calib3d.hpp>
+//#include <opencv2/aruco_samples_utlity.hpp>
 //#include <opencv2/objdetect/aruco_dictionary.hpp>
 
 
@@ -469,9 +473,131 @@ private:
         }
         else if (navigate_type == 7)
         {
-            std::cout << "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFUUUUUUUUUUUUUUUUUUUU-";
-            
-        }
+            std::cout << "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFUUUUUUUUUUUUUUUUUUUU-" << std::endl;
+            cv::VideoCapture inputVideo;
+            inputVideo.open(0);
+            cv::aruco::DetectorParameters detectorParams = cv::aruco::DetectorParameters();
+            cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+            cv::aruco::ArucoDetector detector(dictionary, detectorParams);
+
+
+
+            cv::Size S = cv::Size((int) inputVideo.get(cv::CAP_PROP_FRAME_WIDTH),    // Acquire input size
+                (int) inputVideo.get(cv::CAP_PROP_FRAME_HEIGHT));
+
+            //cv::VideoWriter writer;
+            //int codec = cv::VideoWriter::fourcc('a', 'v', 'c', '1');
+            //double fps = 15.0;
+            //std::string filename = "footage.mp4";
+            //cv::Size sizeFrame(640,480);
+            //writer.open(filename, codec, fps, sizeFrame, true);
+            //cv::Mat cameraMatrix, distCoeffs;
+            //float markerLength = 0.05;
+
+            //readCameraParameters(cameraParamsFilename, cameraMatrix, distCoeffs);
+
+            //cv::Mat objPoints(4, 1, CV_32FC3);
+            //objPoints.ptr<cv::Vec3f>(0)[0] = cv::Vec3f(-markerLength/2.f, markerLength/2.f, 0);
+            //objPoints.ptr<cv::Vec3f>(0)[1] = cv::Vec3f(markerLength/2.f, markerLength/2.f, 0);
+            //objPoints.ptr<cv::Vec3f>(0)[2] = cv::Vec3f(markerLength/2.f, -markerLength/2.f, 0);
+            //objPoints.ptr<cv::Vec3f>(0)[3] = cv::Vec3f(-markerLength/2.f, -markerLength/2.f, 0);
+
+            cv::Mat image, imageCopy;
+            std::vector<int> ids;
+            std::vector<std::vector<cv::Point2f>> corners, rejected;
+            inputVideo >> image;
+            std::cout << "Video Prepared" << std::endl;
+
+
+            cv::Mat res;
+            std::vector<cv::Mat> spl;
+            cv::VideoWriter outputVideo;    
+            int codec = cv::VideoWriter::fourcc('a', 'v', 'i', '1');  // select desired codec (must be available at runtime)
+            double fps = 25.0;                          // framerate of the created video stream
+            std::string filename = "./live.avi";             // name of the output video file
+            outputVideo.open(filename, codec, fps, image.size(), true);
+            // check if we succeeded
+            if (!outputVideo.isOpened()) {
+                std::cerr << "Could not open the output video file for write\n";
+                
+                }
+
+
+
+            std::cout << "Output prepared" << std::endl;
+
+            while (inputVideo.grab()) 
+                {
+                
+                cv::Mat image, imageCopy;
+                inputVideo.retrieve(image);
+                image.copyTo(imageCopy);
+                //std::vector<int> ids;
+                //std::vector<std::vector<cv::Point2f>> corners, rejected;
+                detector.detectMarkers(image, corners, ids, rejected);
+                // if at least one marker detected
+                if (ids.size() > 0)
+                    cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
+
+                //int nMarkers = corner.size();
+                //std::vector<cv::Vec3d> rvecs(nMarkers), tvecs(nMarkers);
+
+                //Calculate pose for each marker
+                //for (int i = 0; i < nMarkers; i++)
+                //{
+                //   solvePnP(objPoints, corner.at(i), cameraMatrix, distCoeffs, rvecs.at(i), tvecs.at(i));
+                //}
+
+                //Draw Axis for each marker
+                //for (unsigned int i = 0; i < ids.size(); i++)
+                //{
+                //    cv::drawFrameAxes(imageCopy, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1)
+                //}
+
+                outputVideo.write(imageCopy);
+
+
+
+
+                cv::imshow("out", imageCopy);
+                //inputVideo >> imageCopy;
+                //cv::Mat xframe;
+                //resize(imageCopy, xframe, sizeFrame);
+                //writer.write(xframe);
+
+
+                //split(src, spl);                // process - extract only the correct channel
+                //for (int i =0; i < 3; ++i)
+                //{
+                    
+                //spl[i] = cv::Mat::zeros(S, spl[0].type());
+                //    
+                //}
+                //merge(spl, res);
+
+               // outputVideo.write(imageCopy);
+                //outputVideo.write(res); //save or
+                //outputVideo << res;
+
+                //int waitTime = 1;
+                char key = (char) cv::waitKey(1);
+                if (key == 27)
+                    break;
+
+                }
+               
+            //bool isSuccess = imwrite("./MyImage.jpg", imageCopy); //write the image to a file as JPEG 
+            //bool isSuccess = imwrite("D:/MyImage.png", image); //write the image to a file as PNG
+            //if (isSuccess == false)
+            //{
+            //std::cout << "Failed to save the image" << std::endl;
+            //std::cin.get(); //wait for a key press
+            //}
+
+            std::cout << "Finished filming!" << std::endl;
+            inputVideo.release();
+            //writer.release();
+        }   
 
 
         // Pause before stopping 
@@ -545,11 +671,7 @@ int main(int argc, char **argv)
     cv::imwrite("marker1.png", markerImage);
 
     //Camera stuff for OpenCV
-    cv::VideoCapture inputVideo;
-    inputVideo.open(0);
-    cv::aruco::DetectorParameters detectorParams = cv::aruco::DetectorParameters();
-    cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
-    cv::aruco::ArucoDetector detector(dictionary, detectorParams);
+    
 
 
 
