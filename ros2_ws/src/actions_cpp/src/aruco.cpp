0c484638d01336f329d3a2f67b13ccfd53ec3d92
Daegan Brown <daeganbrown03@gmail.com>
2024-05-24 03:29:34 -0400

    before fixing laptop

--- ros2_ws/src/actions_cpp/src/aruco.cpp
+++ ros2_ws/src/actions_cpp/src/aruco.cpp
@@ -18,6 +18,20 @@ using NavigateRover = astra_auto_interfaces::action::NavigateRover;
 using NavigateRoverGoalHandle = rclcpp_action::ClientGoalHandle<NavigateRover>;
 using namespace std::placeholders;
 
+//OpenCV
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/aruco.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/objdetect/aruco_detector.hpp>
+#include <opencv2/calib3d.hpp>
+//#include <opencv2/aruco_samples_utlity.hpp>
+//#include <opencv2/objdetect/aruco_dictionary.hpp>
+using namespace std::chrono_literals;
+
+
+
 class NavigateRoverClientNode : public rclcpp::Node 
 {
 public:
@@ -42,12 +56,23 @@ public:
 
         // Add callbacks
         auto options = rclcpp_action::Client<NavigateRover>::SendGoalOptions();
+        options.feedback_callback =
+            std::bind(&NavigateRoverClientNode::feedback_callback, this, _1, _2);
         options.result_callback = 
             std::bind(&NavigateRoverClientNode::goal_result_callback, this, _1);
 
         // Send the goal
         RCLCPP_INFO(this->get_logger(), "Sending a goal");
         navigate_rover_client_->async_send_goal(goal, options);
+    }
+
+    void feedback_callback(
+    NavigateRoverGoalHandle::SharedPtr,
+    const std::shared_ptr<const NavigateRover::Feedback> feedback)
+    {
+        std::stringstream ss;
+    ss << "Next number in sequence received: ";
+    RCLCPP_INFO(this->get_logger(), "%li", feedback->current_status);
     }
 private:
 
@@ -60,18 +85,103 @@ private:
 
     rclcpp_action::Client<NavigateRover>::SharedPtr navigate_rover_client_;
 };
+//*************************************************************************************************
+//MAIN
+//*************************************************************************************************
 
 int main(int argc, char **argv)
 {
-    int navigate_type;
-    double gps_lat_target;
-    double gps_long_target;
+    //*********************************************************************************************
+    //Variables
+    //*********************************************************************************************
+    int navigate_type;      //Always will be 0, assigned later for clarity
+    double gps_lat_target;  //Actually the y-coordinates of the detected square
+    double gps_long_target; //x-coordinates of the detected square
 
-    //Which type of navigation. See astra_auto_server.cpp for a list of
-    //options. 
-    std::cout << "Input Type:" << std::endl;
-    std::cin >> navigate_type; 
-    std::cout << std::endl; 
+
+    //*********************************************************************************************
+    //OpenCV Aruco Shenanigans
+    //*********************************************************************************************
+    
+    std::cout << "Starting Aruco Detection" << std::endl;
+            int cameraNum = 0;
+            //std::cin >> cameraNum;
+            cv::VideoCapture inputVideo(cameraNum);
+            cv::Mat camMatrix, distCoeffs;
+            
+            
+            //inputVideo.open(cameraNum);
+            cv::aruco::DetectorParameters detectorParams = cv::aruco::DetectorParameters();
+            cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+            cv::aruco::ArucoDetector detector(dictionary, detectorParams);
+            
+            cv::Mat image, imageCopy;
+            std::vector<int> ids;
+            std::vector<std::vector<cv::Point2f>> corners, rejected;
+            inputVideo >> image;
+            std::cout << "Video Prepared" << std::endl;
+
+            cv::Mat res;
+            std::vector<cv::Mat> spl;
+            cv::VideoWriter outputVideo;    
+            int codec = cv::VideoWriter::fourcc('H', '2', '6', '4');  // select desired codec (must be available at runtime)
+            double fps = 25.0;                          // framerate of the created video stream
+            std::string filename = "./live.mp4";             // name of the output video file
+            outputVideo.open(filename, codec, fps, image.size(), true);
+            // check if we succeeded
+            if (!outputVideo.isOpened()) {
+                std::cerr << "Could not open the output video file for write\n";
+                
+                }
+
+
+
+                std::cout << "Output prepared" << std::endl;
+            int iterateIT = 0;
+            while (inputVideo.grab()) 
+                {
+                iterateIT ++;
+                cv::Mat image, imageCopy;
+                inputVideo.retrieve(image);
+                image.copyTo(imageCopy);
+                //std::vector<int> ids;
+                //std::vector<std::vector<cv::Point2f>> corners, rejected;
+                detector.detectMarkers(image, corners, ids, rejected);
+                // if at least one marker detected
+                if (ids.size() > 0)
+                    cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
+                
+                outputVideo.write(imageCopy);
+
+
+
+                
+                cv::imshow("out", imageCopy);
+                
+                char key = (char) cv::waitKey(1);
+                if (key == 27)
+                
+                {
+                    break;
+                }
+                
+                
+                }
+               
+          
+            std::cout << "Finished filming!" << std::endl;
+            inputVideo.release();
+
+
+
+    //*********************************************************************************************
+    //Outputs
+    //*********************************************************************************************
+    //Cancels current goal
+
+    //Sends new goal request
+    navigate_type = 0; 
+    
 
     //The target latitude co-ordinate.
     //8 decimal places
